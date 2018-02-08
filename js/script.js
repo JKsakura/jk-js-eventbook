b01ce547f7a6a72ea273ff970115aed457f705df
Jake Zhong <jklevis88@gmail.com>
2018-02-08 10:41:30 -0500

    v0.0.2
    - replace tags with introduction
    - css
    - Adding New function update

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var noteID = 0;
 
 formEl.addEventListener("submit", function(e) {
     e.preventDefault();
-    var id, title, category, tag, syntax, description;
+    var id, title, category, introduction, syntax, description;
     id = noteID;
     title = document.getElementById("add-title").value;
     category = document.getElementById("add-category").value;
@@ -12,39 +12,53 @@ formEl.addEventListener("submit", function(e) {
     syntax = document.getElementById("add-syntax").value;
     description = document.getElementById("add-description").value;
     
-    addNote(id, title, category, tag, syntax, description);
+    addNote(id, title, category, introduction, syntax, description);
     noteID ++;
 }, false);
 
-function addNote(id, title, category, tag, syntax, description) {
-    var newNote = new note(id, title, category, tag, syntax, description);
-    notes.push(newNote);
-    displayNote(newNote);
-}
 
-function note(id, title, category, tag, syntax, description) {
+// ================================================
+//    DECLARE A NEW NOTE OBJECT
+// ================================================
+function note(id, title, category, introduction, syntax, description) {
     this.id = id;
     this.title = title;
     this.category = category;
-    this.tag = tag;
+    this.introduction = introduction;
     this.syntax = syntax;
     this.description = description;
 }
 
+// ================================================
+//    ADD A NEW NOTE ELEMENT WITH PARAMETERS        
+// ================================================
+
+function addNote(id, title, category, introduction, syntax, description) {
+    var newNote = new note(id, title, category, introduction, syntax, description);
+    notes.push(newNote);
+    displayNote(newNote);
+}
+
+// ================================================
+//    DISPLAY THE ELEMENT WITH NEW DOM STRUCTURE     
+// ================================================
 function displayNote(note) {
     var list = document.getElementById("noteList");
     var listItem = document.createElement("li");
     var itemTop = document.createElement("div");
     var itemBottom = document.createElement("div");
     var itemHeader = document.createElement("div");
-    var itemTitle = document.createElement("h4");
+    var itemTitle = document.createElement("p");
     var itemCategory = document.createElement("h5");
     var itemIntroduction = document.createElement("p");
     var itemSyntax = document.createElement("div");
     var itemDescription = document.createElement("div");
     var itemHeader = document.createElement("div");
     
+    listItem.setAttribute("id", "list-item"+noteID);
+    
     listItem.className = "list-item";
+    itemHeader.className = "item-header";
     itemTop.className = "item-top";
     itemBottom.className = "item-bottom";
     itemHeader.className = "item-header";
@@ -53,23 +67,25 @@ function displayNote(note) {
     itemIntroduction.className = "item-introduction";
     itemSyntax.className = "item-syntax";
     itemDescription.className = "item-description";
-    itemHeader.className = "item-header";
     
+    itemTitle.textContent = note.title;
+    itemCategory.textContent = note.category;
+    itemIntroduction.textContent = note.introduction;
+    itemSyntax.textContent = note.syntax;
+    itemDescription.textContent = note.description;
     
-    itemTitle.textContent = "item-title";
-    itemCategory.textContent = "item-category";
-    itemIntroduction.textContent = "item-introduction";
-    itemSyntax.textContent = "item-syntax";
-    itemDescription.textContent = "item-description";
-    
-    // ITEM TOP
-    itemHeader.appendChild(itemTitle);
-    itemHeader.appendChild(itemCategory);
-    itemTop.appendChild(itemHeader);
-    itemTop.appendChild(itemHeader);
-    
-    
-    listItem.appendChild(itemHeader);
+    /* =========== Item Top ============ */
+    // Item Header
+        itemHeader.appendChild(itemTitle); // Item Title
+        itemHeader.appendChild(itemCategory); // Item Category
+        itemTop.appendChild(itemHeader);
+        itemTop.appendChild(itemIntroduction); // Item Introductio
+    /* =========== Item Bottom ============ */
+        itemBottom.appendChild(itemSyntax); // Item Syntax
+        itemBottom.appendChild(itemDescription); // Item Description
+    /* =========== List Item ============ */
+    listItem.appendChild(itemTop);
+    listItem.appendChild(itemBottom);
     
     list.appendChild(listItem);
 }

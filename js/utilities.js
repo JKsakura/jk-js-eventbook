a874d5c1d26b39ff39bcf2bed1306df3b906236a
Jake Zhong <jklevis88@gmail.com>
2019-01-17 14:01:45 -0500

    v0.3.40
    - jquery error

--- js/utilities.js
+++ js/utilities.js
@@ -1,82 +1,80 @@
-jQuery(function($) {
-	/* ============================================================== */
-	/*    VISUAL PART EVENTS  */
-	/* ============================================================== */
-	var headerToggle,
-		pageToggle,
-		addBtnToggle,
-		cancelBtnToggle;
+/* ============================================================== */
+/*    VISUAL PART EVENTS  */
+/* ============================================================== */
+var headerToggle,
+	pageToggle,
+	addBtnToggle,
+	cancelBtnToggle;
 
-	// TOGGLE FOR APP HEADER
-	headerToggle = (function () {
-		var header = $("#header"),
-			toggle = $("#menu-toggle");
-		return {
-			menuToggle: function () {
-				$(toggle).on("click", function () {
-					$(header).toggleClass("open");
-				});
-			}
-		};
-	}());
+// TOGGLE FOR APP HEADER
+headerToggle = (function () {
+	var header = $("#header"),
+		toggle = $("#menu-toggle");
+	return {
+		menuToggle: function () {
+			$(toggle).on("click", function () {
+				$(header).toggleClass("open");
+			});
+		}
+	};
+}());
 
-	// TOGGLE FOR Page
-	pageToggle = (function() {
-		var pages = [],
-			page1,
-			page2;
-		return {
-			pageInit: function(p) {
-				pages.push(p);
-			},
-			pageForward: function (p1, p2) {
-				page1 = $('.page[data-page="' + p1 + '"]');
-				page2 = $('.page[data-page="' + p2 + '"]');
-				$(page1).addClass("forward");
-				$(page2).addClass("active");
-				$(page2).animate({
-					scrollTop: 0
-				}, 0);
-				pages.push(p2);
-				// console.log(pages);
-			},
-			pageBackward: function () {
-				page1 = $('.page[data-page="' + pages[pages.length - 2] + '"]');
-				page2 = $('.page[data-page="' + pages[pages.length - 1] + '"]');
-				$(page1).removeClass("forward");
-				$(page2).removeClass("active");
-				$(page1).animate({
-					scrollTop: 0
-				}, 0);
-				pages.pop();
-				// console.log(pages);
-			}
-		};
-	}());
+// TOGGLE FOR Page
+pageToggle = (function() {
+	var pages = [],
+		page1,
+		page2;
+	return {
+		pageInit: function(p) {
+			pages.push(p);
+		},
+		pageForward: function (p1, p2) {
+			page1 = $('.page[data-page="' + p1 + '"]');
+			page2 = $('.page[data-page="' + p2 + '"]');
+			$(page1).addClass("forward");
+			$(page2).addClass("active");
+			$(page2).animate({
+				scrollTop: 0
+			}, 0);
+			pages.push(p2);
+			// console.log(pages);
+		},
+		pageBackward: function () {
+			page1 = $('.page[data-page="' + pages[pages.length - 2] + '"]');
+			page2 = $('.page[data-page="' + pages[pages.length - 1] + '"]');
+			$(page1).removeClass("forward");
+			$(page2).removeClass("active");
+			$(page1).animate({
+				scrollTop: 0
+			}, 0);
+			pages.pop();
+			// console.log(pages);
+		}
+	};
+}());
 
-	// TOGGLE FOR ADD BUTTON
-	addBtnToggle = (function () {
-		var addBtn = $("#note-add-btn");
-		return {
-			showBtn: function () {
-				$(addBtn).show();
-			},
-			hideBtn: function () {
-				$(addBtn).hide();
-			}
-		};
-	}());
+// TOGGLE FOR ADD BUTTON
+addBtnToggle = (function () {
+	var addBtn = $("#note-add-btn");
+	return {
+		showBtn: function () {
+			$(addBtn).show();
+		},
+		hideBtn: function () {
+			$(addBtn).hide();
+		}
+	};
+}());
 
-	// TOGGLE FOR CANCEL BUTTON
-	cancelBtnToggle = (function () {
-		var cancelBtn = $("#note-cancel-btn");
-		return {
-			showBtn: function () {
-				$(cancelBtn).show();
-			},
-			hideBtn: function () {
-				$(cancelBtn).hide();
-			}
-		};
-	}());
-});
+// TOGGLE FOR CANCEL BUTTON
+cancelBtnToggle = (function () {
+	var cancelBtn = $("#note-cancel-btn");
+	return {
+		showBtn: function () {
+			$(cancelBtn).show();
+		},
+		hideBtn: function () {
+			$(cancelBtn).hide();
+		}
+	};
+}());
